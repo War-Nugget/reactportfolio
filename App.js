149b5e01ac41a05703edf0d149c1326b572a5482
Sahil Patel <sahildepatel@gmail.com>
2022-07-27 20:44:31 -0400

    FIxing files

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import Header from './components/Header';
-import About from './components/About';
+import Header from './Header';
+import About from './About';
 import Resume from './components/Resume';
 import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import Contact from './Contact';
+import Footer from './Footer';
 import resumeData from './resumeData';
 class App extends Component {
   render() {
