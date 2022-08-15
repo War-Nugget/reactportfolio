cac36c47ee0d65b512f7996bcf5d2714d4b26019
Sahil Patel <97697696+War-Nugget@users.noreply.github.com>
2022-08-15 12:34:55 -0400

    Update Portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -32,7 +32,18 @@ export default class Porfolio extends Component {
                     </a>
                   </div>
                 </div>
-          
+           <div className="columns portfolio-item">
+                  <div className="item-wrap">
+                    <a href="#modal-01">
+                      <img src="images/project2.jpg" className="item-img" alt="hotel"/>
+                      <div className="overlay">
+                        <div className="portfolio-item-meta">
+                          <h5>HVAC Estimator</h5>            
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                
           </div>
         </div>
       </div>
